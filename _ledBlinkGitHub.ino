ec3c735f989438d3fef8b3011ff8e2947bfd5bee
al13nning <153411571+al13nning@users.noreply.github.com>
2023-12-10 00:19:07 -0500

    Update _ledBlinkGitHub.ino

--- _ledBlinkGitHub.ino
+++ _ledBlinkGitHub.ino
@@ -1,9 +1,20 @@
+int ledPins[] = { 4, 5, 9, 10 };
+
 void setup() {
   // put your setup code here, to run once:
-
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(ledPins[i], HIGH);
+  }
+  delay(500);
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(ledPins[i], LOW);
+  }
+  delay(500);
 }
